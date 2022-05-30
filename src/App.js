81a8e1bb6a3c0b676a927373bf6a15ddf50ad0e2
Sergey Denisovich <densergej2016@gmail.com>
2022-05-30 10:22:52 +0300

    save changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div className='App'></div>;
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    const { history } = this.props;
+
+    return (
+      <div className='container'>
+        <h1>Header</h1>
+
+        <Switch>
+          <Route history={history} exact path={'/'}>
+            <h2>Home</h2>
+            <Link to={'/cart'}>To Cart</Link>
+          </Route>
+
+          <Route history={history} path={'/cart'}>
+            <h2>Cart</h2>
+            <Link to={'/'}>home</Link>
+          </Route>
+
+          <Route history={history} path={'*'}>
+            <h2>Not Found</h2>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+import './index.css';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   root
 );
