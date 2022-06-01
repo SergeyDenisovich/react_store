48ea4c6904ef7fdd8d0c6ed865bf9bbab937468d
Sergey Denisovich <densergej2016@gmail.com>
2022-06-01 12:36:31 +0300

    save changes

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
-import nextBtn from '../../assets/images/arrow-next.svg';
-import prevBtn from '../../assets/images/arrow-prev.svg';
+import { throttle } from '../../utils/throttle';
 
 import styles from './Gallery.module.scss';
 
 class Gallery extends Component {
-  state = {
-    gallery: this.props.gallery,
-    index: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      gallery: this.props.gallery,
+      index: 0,
+    };
 
-  componentDidMount() {
+    this.showPrevImageTrottled = throttle(this.showPrevImage, 1000);
+    this.showNextImageTrottled = throttle(this.showNextImage, 1000);
+  }
+
+  calculateImagePosition = () => {
     const lastIndex = this.state.gallery?.length - 1;
 
     if (this.state.index < 0) {
@@ -21,20 +26,68 @@ class Gallery extends Component {
     if (this.state.index > lastIndex) {
       this.setState({ index: 0 });
     }
+  };
+
+  componentDidMount() {
+    this.calculateImagePosition();
   }
 
-  render() {
-    console.log('Gallery: ', this.state);
+  componentDidUpdate(prevState) {
+    if (this.state.index !== prevState.index) {
+      this.calculateImagePosition();
+    }
+  }
 
+  showPrevImage = () => {
+    this.setState((state) => ({ index: state.index - 1 }));
+  };
+
+  showNextImage = () => {
+    this.setState((state) => ({ index: state.index + 1 }));
+  };
+
+  render() {
     return (
       <div className={styles.gallery}>
-        <img src={this.state.gallery?.[0]} alt='' />
+        {this.state.gallery.map((image, imageIndex) => {
+          let position = 'nextSlide';
 
-        <button onClick={() => this.setState((state) => ({ index: state.index - 1 }))} className={styles.prevBtn}>
-          <img src={prevBtn} alt='' />
+          if (imageIndex === this.state.index) {
+            position = 'activeSlide';
+          }
+          if (
+            imageIndex === this.state.index - 1 ||
+            (this.state.index === 0 && imageIndex === this.state.gallery.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return <img className={styles[position]} key={imageIndex} src={image} alt='Product' />;
+        })}
+
+        <button onClick={this.showPrevImageTrottled} className={styles.prevBtn}>
+          <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <rect width='24' height='24' fill='black' fill-opacity='0.73' />
+            <path
+              d='M14.25 6.06857L8.625 11.6876L14.25 17.3066'
+              stroke='white'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            />
+          </svg>
         </button>
-        <button onClick={() => this.setState((state) => ({ index: state.index + 1 }))} className={styles.nextBtn}>
-          <img src={nextBtn} alt='' />
+        <button onClick={this.showNextImageTrottled} className={styles.nextBtn}>
+          <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <rect width='24' height='24' transform='matrix(-1 0 0 1 24 0)' fill='black' fill-opacity='0.73' />
+            <path
+              d='M9.75 6.06857L15.375 11.6876L9.75 17.3066'
+              stroke='white'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            />
+          </svg>
         </button>
       </div>
     );
