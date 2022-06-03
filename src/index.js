210de446ea47dcb0fa315db08c41d6955e4fe095
Sergey Denisovich <densergej2016@gmail.com>
2022-06-03 13:04:40 +0300

    save changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/index';
+import store from './store/index';
 import './styles/index.scss';
 
 const root = document.getElementById('root');
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </Router>
   </React.StrictMode>,
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
 import categoryReducer from './slices/categorySlice';
 import currencyReducer from './slices/currencySlice';
 import cartReducer from './slices/cartSlice';
 
-export const store = configureStore({
-  reducer: {
-    category: categoryReducer,
-    currency: currencyReducer,
-    cart: cartReducer,
-  },
+const rootReducer = combineReducers({
+  category: categoryReducer,
+  currency: currencyReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
